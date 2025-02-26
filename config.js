713cbf2e8f07761f5d0962c58586683b734e6226
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-02-26 18:55:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "OQh0kbYD#E4lvHj2Edolaa3V1kp5hNyJaHuFposcg7ifuV0Dkesk",
     CAPTION: process.env.CAPTION || "POWERED BY 𝕬𝖛𝖊.𝕭",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
